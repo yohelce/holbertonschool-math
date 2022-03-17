f10ba94a9be7b77b47be36a95e4b95215d5c2c30
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-17 15:07:09 -0700

    Added function 0-display.c

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+void display_complex_number(complex c)
+{
+	if (c.re && c.im)
+	{
+		if (c.im > 0)
+			printf("%d + %di\n", c.re, c.im);
+		if (c.im == 0)
+			printf("%d\n", c.re);
+		if (c.im < 0)
+			printf("%d - %di\n", c.re, -(c.im));
+	}
+	else if (c.re && c.im == '\0')
+		printf("%d\n", c.re);
+	else if (c.re == '\0' && c.im)
+	{
+		if (c.im > 0)
+			printf("%di\n", c.im);
+		if (c.im == 0)
+			printf("%d\n", c.im);
+		if (c.im < 0)
+			printf("-%di\n", -(c.im));
+	}
+	if (!c.re && !c.im)
+		printf("%d\n", 0);
+}
