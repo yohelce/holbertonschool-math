0a88178e203ec8ecde9afcab2146e41da78ede7a
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-17 15:08:06 -0700

    Added declarations holberton.h

---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,21 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+/**
+ * struct complex - math complex
+ * @re: real number
+ * @im: imaginary number
+ *
+ * Description: number structure for complex mathematical operations
+ */
+
+typedef struct complex
+{
+	int re;
+	int im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif /*HOLBERTON_H*/
