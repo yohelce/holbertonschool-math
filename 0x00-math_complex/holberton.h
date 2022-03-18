fca87b8ee91a2ff900d75c98ffb5845d9464152f
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-18 13:43:39 -0700

    Added new prototype

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -20,5 +20,6 @@ void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
 double power(double a, double n);
+double argument(complex c);
 
 #endif /*HOLBERTON_H*/
