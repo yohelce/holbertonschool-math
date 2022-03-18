4f00ac373ebe4b2e39ca1cfd9ccef56e9fa825bf
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-17 22:55:11 -0700

    Fixed cases of i = 1 or i = -1

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -9,26 +9,44 @@
 
 void display_complex_number(complex c)
 {
-	if (c.re && c.im)
+	if (c.re != 0 && c.im != 0)
 	{
 		if (c.im > 0)
-			printf("%.0f + %.0fi\n", c.re, c.im);
-		if (c.im == 0)
-			printf("%.0f\n", c.re);
+		{
+			if (c.im != 1)
+				printf("%.0f + %.0fi\n", c.re, c.im);
+			else
+				printf("%.0f + i\n", c.re);
+		}
 		if (c.im < 0)
-			printf("%.0f - %.0fi\n", c.re, -(c.im));
+		{
+			if (c.im != -1)
+				printf("%.0f - %.0fi\n", c.re, -(c.im));
+			else
+				printf("%.0f - i\n", c.re);
+		}
 	}
-	else if (c.re && c.im == '\0')
+	else if (c.re && c.im == 0)
 		printf("%.0f\n", c.re);
-	else if (c.re == '\0' && c.im)
+	else if (c.re == 0 && c.im)
 	{
 		if (c.im > 0)
-			printf("%.0fi\n", c.im);
+		{
+			if (c.im != 1)
+				printf("%.0fi\n", c.im);
+			else
+				printf("i");
+		}
 		if (c.im == 0)
 			printf("%.0f\n", c.im);
 		if (c.im < 0)
-			printf("-%.0fi\n", -(c.im));
+		{
+			if (c.im != -1)
+				printf("-%.0fi\n", -(c.im));
+			else
+				printf("-i");
+		}
 	}
-	if (!c.re && !c.im)
+	if (c.re == 0 && c.im == 0)
 		printf("%d\n", 0);
 }
