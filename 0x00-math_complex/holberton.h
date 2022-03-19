8b21b175d9cb5d5b5f385b8bab7cb570cd1b856c
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-18 21:40:42 -0700

    Added new prototype

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -24,5 +24,6 @@ double argument(complex c);
 void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
+void division(complex c1, complex c2, complex *c3);
 
 #endif /*HOLBERTON_H*/
