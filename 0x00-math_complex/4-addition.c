ea386b783d5950c94e7bd658c5d178254116c757
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-18 21:07:05 -0700

    Added function 4-addition.c

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * addition - performs the addition operation to complex numbers
+ * @c1: first complex number
+ * @c2: second complex number
+ * @c3: address of the return
+ *
+ * Return: void
+ */
+
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
