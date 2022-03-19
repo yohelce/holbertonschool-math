644c7e2da3a262a4d6738f9565a551b54f925bc6
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-18 21:37:17 -0700

    Added function 6-multiplication.c

---
+++ 0x00-math_complex/6-multiplication.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * multiplication - performs the multiplication operation to
+ * complex numbers
+ * @c1: first complex number
+ * @c2: second complex number
+ * @c3: address of the multiplication
+ *
+ * Return: void
+ */
+
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
+	c3->im = (c1.re * c2.im) + (c1.im * c2.re);
+}
